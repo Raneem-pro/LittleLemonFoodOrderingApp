eb809043f79d279605140e9e503fa098987cdd73
Raneem <83226325+Raneem-pro@users.noreply.github.com>
2024-05-10 23:43:20 +0300

    start the project

---
+++ LittleLemonFoodOrderingApp/LittleLemonFoodOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonFoodOrderingAppApp.swift
+//  LittleLemonFoodOrderingApp
+//
+//  Created by رنيم القرني on 01/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonFoodOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
